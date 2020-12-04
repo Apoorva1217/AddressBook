591b4e39eac6e831d66a6cc25b7c5fe38577a2d6
Apoorva1217 <62049073+Apoorva1217@users.noreply.github.com>
2020-12-04 16:54:23 +0530

    .

--- push.sh
+++
@@ -1,9 +0,0 @@
-read a
-git branch $a
-git checkout $a
-git add .
-git commit -m "[Apoorva] Add : Ability to get number of contact persons i.e. count by City or State"
-git push origin $a
-git checkout master
-git merge $a
-git push origin master --force
